4885cc32a2a644ea645bc8d89736656eadafe693
林江閤 <tkmk7q8qq@gmail.com>
2020-10-13 20:15:01 +0800

    Add log

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ require "../vendor/autoload.php";
 use \Demo\HelloWorld as World;
 use Demo\Hello\Lara;
 use Demo\Hello;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 $world= new World();
 
@@ -14,3 +16,11 @@ $vincent=new Hello\Someone('Vincent');
 $mary=new Hello\Someone('Mary');
 
 $john=new Hello\Someone('John');
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('../log/my.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
